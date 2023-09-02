9a81ed26b1a39b8a741db56bc8d2a7e481e0d2bf
Rahil CHUGHTAI <rahil.chughtai@amadeus.com>
2023-09-02 12:47:29 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import './App.css';
+import * as Y from 'yjs';
+import { WebrtcProvider } from 'y-webrtc';
+
+import Room from './components/room/Room';
+import { playerType } from './shared/model';
+
+function App() {
+  const [roomInput, setRoomInput] = useState('');
+  const rand = (4000 + Math.floor(Math.random() * 1000)).toString();
+  const [roomCode, setRoomCode] = useState(rand.toString());
+  const [room, setRoom] = useState<WebrtcProvider | null>(null);
+  const [sharedDoc, setSharedDoc] = useState<Y.Doc | null>(null);
+  const [playerType, setPlayerType] = useState<playerType>('host');
+
+  // Function to create a new room
+  const createRoom = async () => {
+    const ydoc = new Y.Doc();
+    const provider = new WebrtcProvider('sentence-game', ydoc, {
+      password: roomCode,
+    });
+    setSharedDoc(ydoc);
+    setRoom(provider);
+  };
+
+  // Function to join an existing room
+  const joinRoom = async () => {
+    const ydoc = new Y.Doc();
+    const provider = new WebrtcProvider('sentence-game', ydoc, {
+      password: roomInput,
+    });
+    setRoomCode(roomInput);
+    setSharedDoc(ydoc);
+    setRoom(provider);
+    setPlayerType('participant');
+  };
+
+  return (
+    <div className="App">
+      <h2>Ray Sentence Game</h2>
+      {room ? (
+        <Room
+          player={playerType}
+          yDoc={sharedDoc}
+          room={room}
+          roomCode={roomCode}
+        />
+      ) : (
+        <div>
+          <button onClick={createRoom}>Create Room</button>
+          <h3>OR</h3>
+          <input
+            type="text"
+            placeholder="Enter Room Code"
+            value={roomInput}
+            onChange={(e) => setRoomInput(e.target.value)}
+          />
+          <button onClick={joinRoom}>Join Room</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/room/Room.tsx
@@ -0,0 +1,115 @@
+import * as Y from 'yjs';
+import { WebrtcProvider } from 'y-webrtc';
+import { useRef, useState } from 'react';
+import { playerType } from '../../shared/model';
+
+interface RoomProps {
+  roomCode: string;
+  yDoc: Y.Doc | null;
+  room: WebrtcProvider;
+  player: playerType;
+}
+interface SentenceData {
+  fragment: string;
+  player: playerType;
+}
+const Room = ({ roomCode, yDoc, room, player }: RoomProps) => {
+  console.log(roomCode, yDoc, room);
+  const questions = [
+    'Who?',
+    'What are they doing?',
+    'In what way/manner?',
+    'Where?',
+    'Why?',
+  ];
+  const COLORS = ['magenta', 'cyan', '#79155B', '#C23373', '#F6635C'];
+  const yArray = yDoc?.getArray<SentenceData>('sentence-array');
+  const [sentences, setSentences] = useState<SentenceData[] | undefined>(
+    yArray?.toArray()
+  );
+  const getColor = (index: number) => {
+    const order = player === 'host' ? 0 : 1;
+    const myColors = COLORS.filter((_, index) => index % 2 === order);
+    console.log(order);
+
+    return myColors[index];
+  };
+
+  const isMyTurn = () => {
+    if (sentences?.length === 0) {
+      return player === 'host';
+    }
+    return sentences?.at(sentences.length - 1)?.player != player;
+  };
+  const myTurn = isMyTurn();
+
+  let finished = sentences?.length === questions.length;
+  const index = sentences?.length || 0;
+
+  const myFragments = sentences
+    ?.filter((val) => val.player === player)
+    .map((data) => data.fragment);
+  const allFragments = sentences?.map((data) => data.fragment);
+
+  const [fragmentInput, setFragmentInput] = useState('');
+
+  yArray?.observe(() => {
+    setSentences(yArray.toArray());
+  });
+
+  const buttonAdd = () => {
+    console.log('hey');
+    const fragment = fragmentInput;
+    yArray?.insert(yArray.toArray().length, [{ fragment, player }]);
+  };
+
+  const resetClick = () => {
+    yArray?.delete(0, sentences?.length);
+    finished = false;
+  };
+  return (
+    <>
+      <h5>You are a {player}</h5>
+      <h5>
+        Your Room Code: <span style={{ color: 'magenta' }}> {roomCode} </span>{' '}
+      </h5>
+      {!finished && (
+        <div>
+          <h3>Type in a sentence fragment that answers the question!</h3>
+          <p
+            style={{
+              fontSize: '1.5em',
+              fontWeight: 'bold',
+              color: COLORS[index],
+            }}
+          >
+            {questions[index]}
+          </p>
+          <input
+            style={{ marginRight: '1em', marginBottom:'1em' }}
+            onChange={(e) => setFragmentInput(e.target.value)}
+          />
+          <button disabled={!myTurn || !fragmentInput} onClick={buttonAdd}>
+            Add sentence fragment
+          </button>
+        </div>
+      )}
+
+      <h3>Your words:</h3>
+      {myFragments?.map((frag, i) => (
+        <p style={{ color: getColor(i) }}>{frag}</p>
+      ))}
+      {finished && (
+        <>
+          <h3 style={{ marginTop: '2em' }}>Final Sentence:</h3>
+          <div>{allFragments?.join(' ')}</div>
+          <button style={{ marginTop: '2em' }} onClick={resetClick}>
+            Reset
+          </button>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Room;
