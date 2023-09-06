504e37069ff006421046980e9464d3fb31c5a3d5
Rahil CHUGHTAI <rahil.chughtai@amadeus.com>
2023-09-06 15:46:27 +0200

    socket

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'https://sentence-socketio.adaptable.app:80';
+const URL = 'https://sentence-socketio.adaptable.app';
 
 export const socket = io(URL);
